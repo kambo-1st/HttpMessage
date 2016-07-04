8d9be19ac178a7e9aaca390928a2c9a1b880c2dc
Bohuslav Šimek <bohuslav@simek.si>
2016-07-04 18:35:20 +0200

    Improve documentation

--- src/UploadedFile.php
+++ src/UploadedFile.php
@@ -26,24 +26,68 @@ use Kambo\HttpMessage\Utils\UploadFile;
  */
 class UploadedFile implements UploadedFileInterface
 {
-
+    /**
+     * Flag signaling that the file has been moved
+     *
+     * @var boolean
+     */
     private $moved = false;
+
+    /**
+     * Size of the file
+     *
+     * @var int|null
+     */
     private $size;
+
+    /**
+     * File error code
+     *
+     * @var int
+     */
     private $error;
+
+    /**
+     * The file media type provided by the client.
+     *
+     * @var string|null
+     */
     private $clientMediaType;
+
+    /**
+     * The filename provided by the client.
+     *
+     * @var string|null
+     */
     private $clientName;
+
+    /**
+     * The full path to the uploaded file provided by the client.
+     *
+     * @var string
+     */
     private $file;
 
+    /**
+     * Stream representation of the uploaded file.
+     *
+     * @var StreamInterface
+     */
     private $stream;
 
+    /**
+     * Simple wrapper for methods connected with upload eg.: is_uploaded_file
+     *
+     * @var UploadFile
+     */
     private $uploadFile;
 
     /**
      * Construct a new UploadedFile instance.
      *
      * @param string      $file            The full path to the uploaded file provided by the client.
-     * @param string|null $clientName      The file name.
-     * @param string|null $clientMediaType The file media type.
+     * @param string|null $clientName      The filename provided by the client.
+     * @param string|null $clientMediaType The file media type provided by the client.
      * @param int|null    $size            The file size in bytes.
      * @param int         $error           The UPLOAD_ERR_XXX code representing the status of the upload.
      */
@@ -60,7 +104,7 @@ class UploadedFile implements UploadedFileInterface
         $this->clientMediaType = $clientMediaType;
         $this->size            = $size;
         $this->error           = $error;
-        
+
         if (isset($uploadFile)) {
             $this->uploadFile = $uploadFile;
         } else {
@@ -71,18 +115,19 @@ class UploadedFile implements UploadedFileInterface
     /**
      * Retrieve a stream representing the uploaded file.
      *
-     * This method MUST return a StreamInterface instance, representing the
+     * This method return a StreamInterface instance, representing the
      * uploaded file. The purpose of this method is to allow utilizing native PHP
      * stream functionality to manipulate the file upload, such as
      * stream_copy_to_stream() (though the result will need to be decorated in a
      * native PHP stream wrapper to work with such functions).
      *
-     * If the moveTo() method has been called previously, this method MUST raise
+     * If the moveTo() method has been called previously, this method will raise
      * an exception.
      *
      * @return StreamInterface Stream representation of the uploaded file.
+     *
      * @throws \RuntimeException in cases when no stream is available or can be
-     *     created.
+     *                           created.
      */
     public function getStream()
     {
@@ -110,13 +155,13 @@ class UploadedFile implements UploadedFileInterface
      * relative path, resolution should be the same as used by PHP's rename()
      * function.
      *
-     * The original file or stream MUST be removed on completion.
+     * The original file or stream is removed on completion.
      *
      * If this method is called more than once, any subsequent calls MUST raise
      * an exception.
      *
      * When used in an SAPI environment where $_FILES is populated, when writing
-     * files via moveTo(), is_uploaded_file() and move_uploaded_file() SHOULD be
+     * files via moveTo(), is_uploaded_file() and move_uploaded_file() are
      * used to ensure permissions and upload status are verified correctly.
      *
      * If you wish to move to a stream, use getStream(), as SAPI operations
@@ -124,10 +169,12 @@ class UploadedFile implements UploadedFileInterface
      *
      * @see http://php.net/is_uploaded_file
      * @see http://php.net/move_uploaded_file
+     *
      * @param string $targetPath Path to which to move the uploaded file.
+     *
      * @throws \InvalidArgumentException if the $path specified is invalid.
-     * @throws \RuntimeException on any error during the move operation, or on
-     *     the second or subsequent call to the method.
+     * @throws \RuntimeException         on any error during the move operation, or on
+     *                                   the second or subsequent call to the method.
      */
     public function moveTo($targetPath)
     {
@@ -151,11 +198,11 @@ class UploadedFile implements UploadedFileInterface
 
         $this->moved = true;
     }
-    
+
     /**
      * Retrieve the file size.
      *
-     * Implementations SHOULD return the value stored in the "size" key of
+     * Implementations returns the value stored in the "size" key of
      * the file in the $_FILES array if available, as PHP calculates this based
      * on the actual size transmitted.
      *
@@ -165,26 +212,26 @@ class UploadedFile implements UploadedFileInterface
     {
         return $this->size;
     }
-    
+
     /**
      * Retrieve the error associated with the uploaded file.
      *
-     * The return value MUST be one of PHP's UPLOAD_ERR_XXX constants.
+     * The return value is one of PHP's UPLOAD_ERR_XXX constants.
      *
-     * If the file was uploaded successfully, this method MUST return
-     * UPLOAD_ERR_OK.
+     * If the file was uploaded successfully, this method return UPLOAD_ERR_OK.
      *
-     * Implementations SHOULD return the value stored in the "error" key of
+     * Implementation returns the value stored in the "error" key of
      * the file in the $_FILES array.
      *
      * @see http://php.net/manual/en/features.file-upload.errors.php
+     *
      * @return int One of PHP's UPLOAD_ERR_XXX constants.
      */
     public function getError()
     {
         return $this->error;
     }
-    
+
     /**
      * Retrieve the filename sent by the client.
      *
@@ -192,17 +239,17 @@ class UploadedFile implements UploadedFileInterface
      * a malicious filename with the intention to corrupt or hack your
      * application.
      *
-     * Implementations SHOULD return the value stored in the "name" key of
+     * Implementation returns the value stored in the "name" key of
      * the file in the $_FILES array.
      *
      * @return string|null The filename sent by the client or null if none
-     *     was provided.
+     *                     was provided.
      */
     public function getClientFilename()
     {
         return $this->clientName;
     }
-    
+
     /**
      * Retrieve the media type sent by the client.
      *
@@ -210,11 +257,11 @@ class UploadedFile implements UploadedFileInterface
      * a malicious media type with the intention to corrupt or hack your
      * application.
      *
-     * Implementations SHOULD return the value stored in the "type" key of
+     * Implementation returns the value stored in the "type" key of
      * the file in the $_FILES array.
      *
      * @return string|null The media type sent by the client or null if none
-     *     was provided.
+     *                     was provided.
      */
     public function getClientMediaType()
     {
